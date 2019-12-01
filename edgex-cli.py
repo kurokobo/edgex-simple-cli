6a6e64cf5a1aa1fdeef3995986688158fca297fd
kurokobo <kuro664@gmail.com>
2019-12-01 14:48:51 +0000

    Add simple cli script

---
+++ edgex-cli.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import fire
+import requests
+import json
+
+class Addressable(object):
+    def list(self, host, port=48081, raw=False):
+        url = "http://%s:%s/api/v1/addressable" % (host, port)
+        list_by_url(url, raw)
+
+    def delete(self, host, id, port=48081):
+        url = "http://%s:%s/api/v1/addressable/id/%s" % (host, port, id)
+        delete_by_url(url)
+
+class ValueDescriptor(object):
+    def list(self, host, port=48080, raw=False):
+        url = "http://%s:%s/api/v1/valuedescriptor" % (host, port)
+        list_by_url(url, raw)
+
+    def delete(self, host, id, port=48080):
+        url = "http://%s:%s/api/v1/valuedescriptor/id/%s" % (host, port, id)
+        delete_by_url(url)
+
+class DeviceProfile(object):
+    def list(self, host, port=48081, raw=False):
+        url = "http://%s:%s/api/v1/deviceprofile" % (host, port)
+        list_by_url(url, raw)
+
+    def delete(self, host, id, port=48081):
+        url = "http://%s:%s/api/v1/deviceprofile/id/%s" % (host, port, id)
+        delete_by_url(url)
+
+class DeviceService(object):
+    def list(self, host, port=48081, raw=False):
+        url = "http://%s:%s/api/v1/deviceservice" % (host, port)
+        list_by_url(url, raw)
+
+    def delete(self, host, id, port=48081):
+        url = "http://%s:%s/api/v1/deviceservice/id/%s" % (host, port, id)
+        delete_by_url(url)
+
+class Device(object):
+    def list(self, host, port=48081, raw=False):
+        url = "http://%s:%s/api/v1/device" % (host, port)
+        list_by_url(url, raw)
+    
+    def delete(self, host, id, port=48081):
+        url = "http://%s:%s/api/v1/device/id/%s" % (host, port, id)
+        delete_by_url(url)
+
+class EdgeX(object):
+    """
+    A Simple CLI Tool for EdgeX Founrdy
+    """
+    def __init__(self):
+        self.addressable = Addressable()
+        self.valuedescriptor = ValueDescriptor()
+        self.deviceprofile = DeviceProfile()
+        self.deviceservice = DeviceService()
+        self.device = Device()
+
+def list_by_url(url, raw=False):
+    response = requests.get(url)
+    data = response.json()
+
+    if raw == False:
+        for item in data:
+            print([item["id"], item["name"]])
+    else:
+        print(json.dumps(data, indent=2))
+
+def delete_by_url(url):
+    response = requests.delete(url)
+    print("[%s] %s" % (response.status_code, response.text))
+
+if __name__ == '__main__':
+    fire.Fire(EdgeX)
